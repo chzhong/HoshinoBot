94e51ffa2990bdb6db18b18046eb195d092e2984
Albert Zhong <hust.zcheng@gmail.com>
2021-09-25 00:35:52 +0800

    解封助手修正bug

--- hoshino/modules/hourcallyao/hourcall_children.py
+++ hoshino/modules/hourcallyao/hourcall_children.py
@@ -7,11 +7,16 @@ import random
 from datetime import date, datetime, timedelta
 from hoshino import util
 from hoshino import R
-from hoshino.priv import check_priv, SUPERUSER
 from hoshino.service import Service, priv
 
 sv_help = '''
 未成年解放提醒的说
+- 封印 @被封印者 [@代理人 [代理星期]]
+- 解封 @被封印者
+- 强化结界 日期
+- 削弱结界 日期
+- 查看结界
+- 查看封印
 '''.strip()
 
 sv = Service(
@@ -65,10 +70,11 @@ class SealConfig:
         self._load_config(path)
 
     def _load_config(self, path='config.json'):
-        self.path = path
-        if os.path.exists(path):
-            with open(path, 'r') as fp:
-                config = json.load()
+        self.path = os.path.join(os.path.dirname(__file__), path)
+        sv.logger.info("加载配置 " + self.path)
+        if os.path.exists(self.path):
+            with open(self.path, 'r') as fp:
+                config = json.load(fp)
         else:
             config = {}
         exceptions = config.get('exceptions', {})
@@ -89,6 +95,7 @@ class SealConfig:
                 'seals': self.seals
             }
             json.dump(config, fp)
+            fp.flush()
 
     def add_seal(self, gid: str, qqid: str, delegate=None, delegate_days=None):
         group_config = self.seals.get(gid, {})
@@ -138,7 +145,7 @@ class SealConfig:
             changed = True
         if changed:
             self.save_config()
-        return changed
+        return black_date
 
     def add_free_date(self, s, now=None):
         free_date = parse_date(s, now).strftime('%Y-%m-%d')
@@ -151,35 +158,56 @@ class SealConfig:
             changed = True
         if changed:
             self.save_config()
-        return changed
+        return free_date
 
     def clean_dates(self):
         today = date.today()
-        self.free_dates[:] = filter(lambda d: datetime(d, '%Y-%m-%d').date() >= today, self.free_dates)
-        self.black_dates[:] = filter(lambda d: datetime(d, '%Y-%m-%d').date() >= today, self.black_dates)
+        self.free_dates[:] = list(filter(lambda d: datetime.strptime(d, '%Y-%m-%d').date() >= today, self.free_dates))
+        self.black_dates[:] = list(filter(lambda d: datetime.strptime(d, '%Y-%m-%d').date() >= today, self.black_dates))
+        self.save_config()
 
     def _at_sealed(self, dow: int, free_day, qqid, seal_config):
         if 'delegate' not in seal_config:
             return atqq(qqid)
         else:
             delegate = seal_config['delegate']
-            delegate_days = seal_config.get('delegate_days', [])
-            should_delegate = not free_day and dow in delegate_days
+            delegate_days = seal_config.get('delegate_days', None)
+            should_delegate = not free_day and (delegate_days is None or dow in delegate_days)
             if should_delegate:
                 return f'{atqq(delegate)}(代 {atqq(qqid)})'
             else:
                 return atqq(qqid)
 
+    def _describe_seal(self, qqid, seal_config):
+        if 'delegate' not in seal_config:
+            return atqq(qqid)
+        else:
+            delegate = seal_config['delegate']
+            delegate_days = seal_config.get('delegate_days', [])
+            if delegate_days:
+                return f'{atqq(qqid)}(代理人： {atqq(delegate)}，代理星期：{",".join(delegate_days)})'
+            else:
+                return f'{atqq(qqid)}(代理人： {atqq(delegate)})'
+
+    def describe_group_seals(self, gid):
+        group_config = self.seals.get(gid)
+        if not group_config:
+            return '无人被封印'
+        seal_descs = []
+        for qqid, seal_config in group_config.items():
+            seal_descs.append(self._describe_seal(qqid, seal_config))
+        return '本群被封印人员：\n' + '\n'.join(seal_descs)
+
     def get_ats(self, dow: int, free_day):
-        ats = []
-        for gid, group_config in self.seals:
-            group_ats = []
-            group_at_config = {'gid': gid, 'ats': group_ats}
-            for qqid, seal_config in group_config:
-                group_ats.append(self._at_sealed(dow, free_day, qqid, seal_config))
-            group_at_config['ats'] = group_ats
-            ats.append(group_at_config)
-        return ats
+            ats = []
+            for gid, group_config in self.seals.items():
+                group_ats = []
+                group_at_config = {'gid': gid, 'ats': group_ats}
+                for qqid, seal_config in group_config.items():
+                    group_ats.append(self._at_sealed(dow, free_day, qqid, seal_config))
+                group_at_config['ats'] = group_ats
+                ats.append(group_at_config)
+            return ats
 
     def is_relax_day(self, now: datetime = None):
         now = now if now else datetime.now()
@@ -207,7 +235,29 @@ async def bangzhu(bot, ev):
     await bot.send(ev, sv_help, at_sender=True)
 
 
-@sv.on_prefix(["封印"])
+@sv.on_fullmatch(["查看封印"])
+async def ls_seals(bot, ev):
+    config = load_config()
+    config.clean_dates()
+    msg = config.describe_group_seals(str(ev.group_id))
+    await bot.send(ev, msg, at_sender=True)
+
+
+@sv.on_fullmatch(["查看结界"])
+async def ls_special_days(bot, ev):
+    config = load_config()
+    config.clean_dates()
+    now = datetime.now(pytz.timezone('Asia/Shanghai'))
+    relax_day, day, dow, free_day = config.is_relax_day(now)
+    msg = f'今日结界状态：\n' \
+          f'解封时段：{"20时会解封1小时" if relax_day else "不会解封"}\n' \
+          f'结界强度: {"强化" if free_day is None else ("脆弱" if free_day else "正常")}\n' \
+          f'结界强化日期：{", ".join(config.black_dates) if config.black_dates else "无"}\n' \
+          f'结界脆弱日期：{", ".join(config.free_dates) if config.free_dates else "无"}\n'
+    await bot.send(ev, msg)
+
+
+@sv.on_prefix("封印")
 async def seal(bot, ev):
     gid = str(ev.group_id)
     sid = None
@@ -215,13 +265,12 @@ async def seal(bot, ev):
     days = ev.message.extract_plain_text()
     delegate_days = None
     for m in ev.message:
+        sv.logger.info(f'Message Type: {m.type}, Data: {json.dumps(m.data)}')
         if m.type == 'at' and m.data['qq'] != 'all':
             if sid is None:
                 sid = m.data['qq']
             elif delegate is None:
                 delegate = m.data['qq']
-        elif m.type == 'at' and m.data['qq'] == 'all':
-            continue
     if delegate and days and days.strip():
         delegate_days = list(map(int, days.split(',')))
     if sid is None:
@@ -231,7 +280,7 @@ async def seal(bot, ev):
     config.add_seal(gid, sid, delegate, delegate_days)
 
 
-@sv.on_prefix(["解封"])
+@sv.on_prefix("解封")
 async def unseal(bot, ev):
     gid = str(ev.group_id)
     ats = []
@@ -240,13 +289,16 @@ async def unseal(bot, ev):
         if m.type == 'at' and m.data['qq'] != 'all':
             sids.append(m.data['qq'])
             ats.append(atqq(m.data['qq']))
+    if sids is None:
+        await bot.send(ev, '请@需要解封的群员哦w')
+        return
     config = load_config()
     for sid in sids:
         config.remove_seal(gid, sid)
     await bot.send(ev, '已解封 ' + ' '.join(ats))
 
 
-@sv.on_prefix(["强化结界"])
+@sv.on_prefix("强化结界")
 async def add_black(bot, ev):
     text = str(ev.message).strip()
     if not text:
@@ -254,13 +306,14 @@ async def add_black(bot, ev):
         return
     cfg = load_config()
     try:
-        cfg.add_black_date(text)
+        day = cfg.add_black_date(text)
+        await bot.send(ev, "已经强化 " + day + " 的结界，当日的封印不会解除。", at_sender=True)
     except ValueError as e:
         await bot.send(ev, str(e) + "\n格式：强化结界 (年/)月/日", at_sender=True)
         return
 
 
-@sv.on_prefix(["削弱结界"])
+@sv.on_prefix("削弱结界")
 async def add_free(bot, ev):
     text = str(ev.message).strip()
     if not text:
@@ -268,7 +321,8 @@ async def add_free(bot, ev):
         return
     cfg = load_config()
     try:
-        cfg.add_free_date(text)
+        day = cfg.add_free_date(text)
+        await bot.send(ev, "已经削弱 " + day + " 的结界，当日的封印被临时解除。", at_sender=True)
     except ValueError as e:
         await bot.send(ev, str(e) + "\n格式：削弱结界 (年/)月/日", at_sender=True)
         return
@@ -289,16 +343,23 @@ async def hour_call():
     if not now.hour == 20:
         return
     config = load_config()
-    relax_day, _, dow, free_day = config.is_relax_day(now)
+    relax_day, day, dow, free_day = config.is_relax_day(now)
     if not relax_day:
+        sv.logger.info(f'今天({day})的结界无法解开')
         return
+    sv.logger.info(f'今天({day})的结界已经解开：周{dow}, 结界脆弱: {free_day}')
     ats = config.get_ats(dow, free_day)
     if not ats:
+        sv.logger.info(f'无群需要提示')
         return
     for group_at_config in ats:
         group_ats = group_at_config['ats']
-        if not group_ats:
-            continue
         gid = group_at_config['gid']
+        if not group_ats:
+            sv.logger.info(f'群 {gid} 无封印人员')
+            continue
+        sv.logger.info(f'向群 {gid} 的 {len(group_ats)} 名被封印人员发送提醒')
         await sv.bot.send_group_msg(int(gid), '封印已经解除，孩子们可以稍微放松一下了。' + ' '.join(group_ats)
-        + str(R.img(f"children_release{random.randint(1, 4)}.jpg").cqcode))
+                                    + str(R.img(f"children_release{random.randint(1, 4)}.jpg").cqcode))
+    sv.logger.info('清理过期的特殊封印时点')
+    config.clean_dates()
